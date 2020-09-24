64759c623e6cf359a3e14321e2ef6f4afc5e635f
unknown <z4ghjdyx@foxmail.com>
2020-09-24 16:28:17 +0800

    test: webpack-base test

--- test/unit/webpack-base.test.js
+++ test/unit/webpack-base.test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js')
   console.log(baseConfig)
   it('entry', () => {
-    assert.strictEqual(baseConfig.entry.index, 'D:/ghj/builder-webpack-ghj/test/smoke/template/src/index/index.js')
-    assert.strictEqual(baseConfig.entry.search, 'D:/ghj/builder-webpack-ghj/test/smoke/template/src/search/index.js')
+    assert.strictEqual(baseConfig.entry.index.includes('test/smoke/template/src/index/index.js'), true)
+    assert.strictEqual(baseConfig.entry.search.includes('test/smoke/template/src/search/index.js'), true)
   })
 })
