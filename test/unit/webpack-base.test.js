62703a68e2ebbae7a322b9a33cc5740bd91f4719
unknown <z4ghjdyx@foxmail.com>
2020-09-24 16:22:00 +0800

    feat: console 脚本

--- test/unit/webpack-base.test.js
+++ test/unit/webpack-base.test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js')
+  console.log(baseConfig)
   it('entry', () => {
     assert.strictEqual(baseConfig.entry.index, 'D:/ghj/builder-webpack-ghj/test/smoke/template/src/index/index.js')
     assert.strictEqual(baseConfig.entry.search, 'D:/ghj/builder-webpack-ghj/test/smoke/template/src/search/index.js')
